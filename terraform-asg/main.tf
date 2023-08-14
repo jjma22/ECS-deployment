9806d260bf052895794fe2a798f85ac5d4b18a2d
jjma22 <joe.adams123@hotmail.co.uk>
2023-08-14 08:42:57 +0100

    uncomment out module/ output

--- terraform-asg/main.tf
+++ terraform-asg/main.tf
@@ -8,16 +8,16 @@ module "create-database" {
     vpc_id = module.create-network.vpc_id
 }
 
-# module "launch-instnace" {
-#     source = "./module/launch-instance"
-#     user-data = local.user_data_base64
-#     vpc_id = module.create-network.vpc_id
-#     ec2-subnet = module.create-network.public-subnet-group-name
-#     public_subnets = module.create-network.public-subnet-group-name
-# }
+module "launch-instnace" {
+    source = "./module/launch-instance"
+    user-data = local.user_data_base64
+    vpc_id = module.create-network.vpc_id
+    ec2-subnet = module.create-network.public-subnet-group-name
+    public_subnets = module.create-network.public-subnet-group-name
+}
 
-# locals {
-#   user_data_file = file("${path.module}/user-data.sh")
-#   user_data_base64 = base64encode(local.user_data_file)
+locals {
+  user_data_file = file("${path.module}/user-data.sh")
+  user_data_base64 = base64encode(local.user_data_file)
   
-# }
+}
--- terraform-asg/output.tf
+++ terraform-asg/output.tf
@@ -1,6 +1,6 @@
-# output "address" {
-#     value = "${module.launch-instnace.ec2-dns}:5000"
-# }
+output "address" {
+    value = "${module.launch-instnace.ec2-dns}:5000"
+}
 
 output "public" {
     value = module.create-network.public-subnet-group-name
